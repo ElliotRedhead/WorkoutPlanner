90d633493bf6dd8d65d4316887ee153db8f32fea
ElliotRedhead <elliot.redhead@gmail.com>
2019-12-21 22:58:44 +0000

    Added environmental variables: mongo credentials.

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import os
 from flask import Flask, redirect, render_template, request, url_for
+from flask_pymongo import PyMongo
+
+
+if path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")  
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
