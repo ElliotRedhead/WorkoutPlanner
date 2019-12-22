90b8046966982f64b77195291f62e4f0d877cd21
ElliotRedhead <elliot.redhead@gmail.com>
2019-12-22 17:02:59 +0000

    Removed mongo get commands.

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ if path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")  
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+# app.config["MONGO_URI"] = os.environ.get("MONGO_URI")  
+# app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 
 @app.route("/")
 def hello():
