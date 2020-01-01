5be5719676b70fe22aefbbb4c74d39ecfbcf2343
ElliotRedhead <elliot.redhead@gmail.com>
2020-01-01 15:08:34 +0000

    Added titles to home and login pages.

--- app.py
+++ app.py
@@ -8,12 +8,23 @@ if path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")  
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 
+
 @app.route("/")
-def hello():
-    return render_template("exercises.html")
+@app.route("/index")
+def homepage():
+    return render_template(
+        "index.html",
+        title = "Workout Planner | Home")
+
+@app.route("/login")
+def login():
+    return render_template(
+        "login.html",
+        title="Workout Planner | Login")
+
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
