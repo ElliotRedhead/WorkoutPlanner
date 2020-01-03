4aa36da2b7fd3bdfb652513dc1cede8dab7906ce
ElliotRedhead <elliot.redhead@gmail.com>
2020-01-03 01:11:28 +0000

    Logged in user is added to the flask Session.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, session
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -9,6 +9,7 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 client = PyMongo(app)
 
@@ -30,22 +31,21 @@ def exercises(owner):
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    users = client.db.users.find()
     if request.method == "POST":
         logged_user = client.db.users.find_one({"username": request.form["inputUsername"]})
         if logged_user:
             # client.db.users.update_one({"username":request.form["inputUsername"]},{ "$set": {"password":generate_password_hash(request.form["inputPassword"])}})
             if check_password_hash((logged_user["password"]),(request.form["inputPassword"])):
                 print("Login complete.")
+                session["username"] = logged_user["username"]
+                return redirect(url_for("homepage"))
             else:
                 print("Invalid credentials provided.")
         else:
             print("Logged user not found.")
     return render_template(
         "login.html",
-        title="Workout Planner | Login",
-        users=users)
-
+        title="Workout Planner | Login")
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
