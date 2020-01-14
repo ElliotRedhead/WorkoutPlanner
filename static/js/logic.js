7c4e0a4f7a3a75872eacc9438279f9e445873910
ElliotRedhead <elliot.redhead@gmail.com>
2020-01-14 17:18:47 +0000

    Bridging login backend with frontend logic.

--- static/js/logic.js
+++ static/js/logic.js
@@ -23,10 +23,48 @@ $(document).ready(function () {
                         .then(
                             responseJSON => {
                                 let alertMessage = "";
-                                if(responseJSON.newUsername == false)
-                                    {alertMessage = alertMessage.concat("Username already exists.<br>");}
-                                if(responseJSON.newEmail == false)
-                                    {alertMessage = alertMessage.concat("Email address already registered.");}
+                                if (responseJSON.newUsername == false) { alertMessage = alertMessage.concat("Username already exists.<br>"); }
+                                if (responseJSON.newEmail == false) { alertMessage = alertMessage.concat("Email address already registered."); }
+                                Swal.fire({
+                                    title: "Registration unsuccessful",
+                                    html: alertMessage,
+                                    confirmButtonText: 'Ok'
+                                })
+                            }
+                        )
+                }
+                if (response.redirected) {
+                    window.location.href = response.url
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    })
+    $("#loginForm").submit(function (event) {
+        event.preventDefault();
+        const inputUsername = ($("#inputUsername")).val();
+        const inputPassword = ($("#inputPassword")).val();
+        const data = {
+            inputUsername: inputUsername,
+            inputPassword: inputPassword
+        };
+        fetch('/register', {
+            method: 'POST',
+            cors: '*same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => {
+                if (response.redirected == false) {
+                    response.json()
+                        .then(
+                            responseJSON => {
+                                let alertMessage = "";
+                                if (responseJSON.newUsername == false) { alertMessage = alertMessage.concat("Username already exists.<br>"); }
+                                if (responseJSON.newEmail == false) { alertMessage = alertMessage.concat("Email address already registered."); }
                                 Swal.fire({
                                     title: "Registration unsuccessful",
                                     html: alertMessage,
