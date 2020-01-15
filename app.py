1ead05fae6226c9b2c324dc326019a1deb8edede
unknown <elliot.redhead@gmail.com>
2020-01-15 21:27:44 +0000

    Autopep8 applied for conventional formatting.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import os, json
+import os
+import json
 from flask import Flask, redirect, render_template, request, url_for, session, jsonify
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -12,15 +13,15 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-
 client = PyMongo(app)
 
+
 @app.route("/")
-@app.route("/index")
 def homepage():
     return render_template(
         "index.html",
-        title = "Workout Planner | Home")
+        title="Workout Planner | Home")
+
 
 @app.route("/myexercises")
 def exercises(owner):
@@ -28,21 +29,26 @@ def exercises(owner):
     return render_template(
         "myexercises.html",
         title="Workout Planner | My Exercises",
-        exercises = exercises)
+        exercises=exercises)
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         request_data = request.get_json()
         print(request_data)
-        response = {"existingUsername":False,"validPassword":False}
-        logged_username = client.db.users.find_one({"username": request_data["inputUsername"]})
+        response = {"existingUsername": False, "validPassword": False}
+        logged_username = client.db.users.find_one(
+            {"username": request_data["inputUsername"]}
+        )
         if logged_username is None:
             response["existingUsername"] = False
             return json.dumps(response)
         else:
             response["existingUsername"] = True
-            if check_password_hash((logged_username["password"]),(request_data["inputPassword"])):
+            if check_password_hash(
+                (logged_username["password"]),
+                    (request_data["inputPassword"])):
                 return redirect(url_for("homepage"))
             else:
                 response["validPassword"] = False
@@ -51,14 +57,17 @@ def login():
         "login.html",
         title="Workout Planner | Login")
 
+
 @app.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == "POST":
         request_data = request.get_json()
         print(request_data)
-        response = {"newUsername":False,"newEmail":False}
-        existing_username = client.db.users.find_one({"username": request_data["inputUsername"]})
-        existing_email = client.db.users.find_one({"email":request_data["inputEmail"]})
+        response = {"newUsername": False, "newEmail": False}
+        existing_username = client.db.users.find_one(
+            {"username": request_data["inputUsername"]})
+        existing_email = client.db.users.find_one(
+            {"email": request_data["inputEmail"]})
         if existing_username is None:
             response["newUsername"] = True
         else:
@@ -68,7 +77,12 @@ def register():
         else:
             response["newEmail"] = False
         if existing_username is None and existing_email is None:
-            client.db.users.insert_one({"username" : request_data["inputUsername"],"email" : request_data["inputEmail"], "password" : generate_password_hash(request_data["inputPassword"])})
+            client.db.users.insert_one(
+                {
+                    "username": request_data["inputUsername"],
+                    "email": request_data["inputEmail"],
+                    "password": generate_password_hash(
+                        request_data["inputPassword"])})
             session["username"] = request_data["inputUsername"]
             return redirect(url_for("login"))
         else:
@@ -76,6 +90,7 @@ def register():
     return render_template(
         "register.html",
         title="Workout Planner | Register")
+
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
