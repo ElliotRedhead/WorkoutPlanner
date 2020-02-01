b01e44b22c7452fedc275355f12fef7eb5f9562b
ElliotRedhead <elliot.redhead@gmail.com>
2020-02-01 23:58:48 +0000

    Refactored based on ESLint results.

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,13 +5,13 @@ function fetchParameterInit (inputData) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(inputData)
     }
-    return fetchParameters;
+    return fetchParameters
 }
 
-function displayModal(modalTitle, modalText = "", pageRedirect = false) {
+function displayModal (modalTitle, modalText = "", pageRedirect = false) {
     Swal.fire({
         title: modalTitle,
-        text: modalText,
+        html: modalText,
         confirmButtonText: "Ok"
     })
         .then(function () {
@@ -23,36 +23,36 @@ function displayModal(modalTitle, modalText = "", pageRedirect = false) {
 
 $(document).ready(function () {
     $("#registerForm").submit(function (event) {
-        event.preventDefault();
+        event.preventDefault()
         const inputData = {
             inputUsername: ($("#inputUsername")).val().toLowerCase(),
             inputEmail: ($("#inputEmail")).val().toLowerCase(),
             inputPassword: ($("#inputPassword")).val()
-        };
+        }
         fetch('/register', fetchParameterInit(inputData))
             .then(response => {
                 response.json()
                     .then(
                         responseJSON => {
                             if (responseJSON.hasOwnProperty("url")) {
-                                window.location.replace(responseJSON.url);
+                                window.location.replace(responseJSON.url)
                             }
                             let alertMessage = "";
-                            if (responseJSON.newUsername == false) { alertMessage = alertMessage.concat("Username already exists.<br>"); }
-                            if (responseJSON.newEmail == false) { alertMessage = alertMessage.concat("Email address already registered."); }
-                            if (responseJSON.newUsername == false || responseJSON.newEmail == false) {
+                            if (responseJSON.newUsername === false) { alertMessage = alertMessage.concat("Username already exists.<br>"); }
+                            if (responseJSON.newEmail === false) { alertMessage = alertMessage.concat("Email address already registered."); }
+                            if (responseJSON.newUsername === false || responseJSON.newEmail === false) {
                                 displayModal("Registration unsuccessful", alertMessage, false)
                             }
                         }
                     )
                     .catch(error => {
-                        console.log(error);
+                        console.log(error)
                     })
             })
     })
     $("#loginForm").submit(function (event) {
-        event.preventDefault();
-        inputData = {
+        event.preventDefault()
+        const inputData = {
             inputUsername: ($("#inputUsername")).val().toLowerCase(),
             inputPassword: ($("#inputPassword")).val().toLowerCase()
         };
@@ -62,37 +62,37 @@ $(document).ready(function () {
                     .then(
                         responseJSON => {
                             if (responseJSON.hasOwnProperty("url")) {
-                                window.location.replace(responseJSON.url);
+                                window.location.replace(responseJSON.url)
                             }
-                            if (responseJSON.existingUsername == false) { alertMessage = ("Invalid username."); }
-                            if (responseJSON.validPassword == false) { alertMessage = ("Invalid password."); }
-                            if (responseJSON.existingUsername == false || responseJSON.validPassword == false) {
+                            if (responseJSON.existingUsername === false) { const alertMessage = ("Invalid username."); }
+                            if (responseJSON.validPassword === false) { const alertMessage = ("Invalid password."); }
+                            if (responseJSON.existingUsername === false || responseJSON.validPassword === false) {
                                 displayModal("Login unsuccessful", alertMessage, false)
                             }
                         })
             }
             )
             .catch(error => {
-                console.log(error);
+                console.log(error)
             })
     })
     $("#createExerciseForm").submit(function (event) {
-        event.preventDefault();
-        let inputData = {};
+        event.preventDefault()
+        const inputData = {};
         ($("input").each(function () {
-            inputData[this.id.toLowerCase()] = this.value.toLowerCase();
-        }));
+            inputData[this.id.toLowerCase()] = this.value.toLowerCase()
+        }))
         fetch("/createexercise", fetchParameterInit(inputData))
             .then(
                 displayModal("Exercise created", undefined, true)
             )
     })
     $("#editExerciseForm").submit(function (event) {
-        event.preventDefault();
-        let inputData = {};
+        event.preventDefault()
+        const inputData = {};
         ($("input").each(function () {
-            inputData[this.id.toLowerCase()] = this.value.toLowerCase();
-        }));
+            inputData[this.id.toLowerCase()] = this.value.toLowerCase()
+        }))
         fetch(window.location.href, fetchParameterInit(inputData))
             .then(
                 displayModal("Exercise created", undefined, true)
