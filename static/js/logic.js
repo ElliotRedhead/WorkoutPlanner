66df35decd9507c41ece11f1dd1f71335af6bdbf
unknown <elliot.redhead@gmail.com>
2020-02-13 07:04:04 +0000

    Formatted with Prettier.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,117 +1,135 @@
 /**
-* Once DOM creation is complete the contained functions are called if
-* their triggering form is submitted.
-*/
-$(document).ready(function () {
-    $("#registerForm").submit(function () {
-        registerFormHandling()
-    })
-    $("#loginForm").submit(function () {
-        loginFormHandling()
-    })
-    $("#createExerciseForm").submit(function () {
-        event.preventDefault()
-        fetch("/createexercise", fetchParameterInit(createExerciseObject()))
-            .then(
-                displayModal("Exercise created", undefined, true)
-            )
-    })
-    $("#editExerciseForm").submit(function () {
-        event.preventDefault()
-
-        fetch(window.location.href, fetchParameterInit(createExerciseObject()))
-            .then(
-                displayModal("Exercise created", undefined, true)
-            )
-    }
-    )
-})
-
-/**
- * 
+ * Once DOM creation is complete the contained functions are called if
+ * their triggering form is submitted.
  */
-function getInputData (emailInputRequired = false) {
+$(document).ready(function() {
+	$("#registerForm").submit(function() {
+		registerFormHandling();
+	});
+	$("#loginForm").submit(function() {
+		loginFormHandling();
+	});
+	$("#createExerciseForm").submit(function() {
+		event.preventDefault();
+		fetch("/createexercise", fetchParameterInit(createExerciseObject())).then(
+			displayModal("Exercise created", undefined, true)
+		);
+	});
+	$("#editExerciseForm").submit(function() {
+		event.preventDefault();
+		fetch(
+			window.location.href,
+			fetchParameterInit(createExerciseObject())
+		).then(displayModal("Exercise created", undefined, true));
+	});
+});
+
+/**
+ *
+ */
+function getInputData(emailInputRequired = false) {
 	const inputData = {
-		inputUsername: ($("#inputUsername")).val().toLowerCase(),
-		inputPassword: ($("#inputPassword")).val(),
+		inputUsername: $("#inputUsername")
+			.val()
+			.toLowerCase(),
+		inputPassword: $("#inputPassword").val()
+	};
+	if (emailInputRequired) {
+		inputData["inputEmail"] = $("#inputEmail")
+			.val()
+			.toLowerCase();
 	}
-	if (emailInputRequired) {inputData["inputEmail"] = ($("#inputEmail")).val().toLowerCase()}
-	return inputData
-	}
+	return inputData;
+}
 
 /**
-* The fields from the registration form are added to an object, if the field's
-* case is not required it is set to lowercase for standardisation.
-* A fetch request is formed containing the submitted data for verification.
-* If submitted credentials are valid: user is redirected to their exercise list.
-* If credentials aren't valid: the invalid input is isolated and passed to a 
-* modal that states the failed input to the user.
-*/
+ * The fields from the registration form are added to an object, if the field's
+ * case is not required it is set to lowercase for standardisation.
+ * A fetch request is formed containing the submitted data for verification.
+ * If submitted credentials are valid: user is redirected to their exercise list.
+ * If credentials aren't valid: the invalid input is isolated and passed to a
+ * modal that states the failed input to the user.
+ */
 function registerFormHandling() {
-    event.preventDefault()
-	inputData = getInputData(true)
-	fetch('/register', fetchParameterInit(inputData))
+	event.preventDefault();
+	const inputData = getInputData(true);
+	fetch("/register", fetchParameterInit(inputData))
 		.then(response => {
-			responseToJson(response, inputData, "register")})
-		.catch(error => {console.log(error)})}
+			responseToJson(response, inputData, "register");
+		})
+		.catch(error => {
+			console.log(error);
+		});
+}
 
+function invalidResponseHandling(resultJson, responseHandlingType) {
+	responseHandlingType == "register"
+		? (isolationNumber = 3)
+		: (isolationNumber = 5);
+	const invalidInput = authBooleanCheck(resultJson, isolationNumber);
+	if (responseHandlingType == "register") {
+		let alertMessage = "";
+		Object.keys(invalidInput).forEach(key => {
+			alertMessage = alertMessage + `${invalidInput[key]} already exists.</br>`;
+		});
+		displayModal("Registration unsuccessful", alertMessage, false);
+	} else if (responseHandlingType == "login") {
+		if (invalidInput.length > 0) {
+			displayModal("Login unsuccessful", `Invalid ${invalidInput}`);
+		}
+	}
+}
 
-function invalidResponseHandling(resultJson, responseHandlingType){
-		responseHandlingType == "register" ? isolationNumber = 3 : isolationNumber = 5;
-		const invalidInput = authBooleanCheck(resultJson, isolationNumber)
-		if (responseHandlingType == "register"){
-			let alertMessage = "";
-			Object.keys(invalidInput).forEach((key => {
-				alertMessage = alertMessage + `${invalidInput[key]} already exists.</br>`
-			}))
-			displayModal("Registration unsuccessful", alertMessage, false)
-		}
-		else if (responseHandlingType == "login"){
-			if (invalidInput.length > 0) {
-				displayModal("Login unsuccessful", `Invalid ${invalidInput}`)
-			}
-		}
-		}
-
-		
 /**
-* Submitted form data is added to an object, if the field's case is 
-* not required it is set to lowercase for standardisation.
-* A fetch request is formed containing the submitted data for verification.
-* If submitted credentials are valid: user is redirected to their exercise list.
-* If credentials aren't valid: the invalid input is isolated and passed to a 
-* modal that states the failed input to the user.
+ * Submitted form data is added to an object, if the field's case is
+ * not required it is set to lowercase for standardisation.
+ * A fetch request is formed containing the submitted data for verification.
+ * If submitted credentials are valid: user is redirected to their exercise list.
+ * If credentials aren't valid: the invalid input is isolated and passed to a
+ * modal that states the failed input to the user.
  */
 function loginFormHandling(inputData) {
-	event.preventDefault()
-	inputData = getInputData()
-    fetch('/login', fetchParameterInit(inputData))
+	event.preventDefault();
+	inputData = getInputData();
+	fetch("/login", fetchParameterInit(inputData))
 		.then(response => {
-			responseToJson(response, inputData, "login")})
-        .catch(error => {console.log(error)})}
+			responseToJson(response, inputData, "login");
+		})
+		.catch(error => {
+			console.log(error);
+		});
+}
 
 function responseToJson(fetchResult, inputData, responseHandlingType) {
-	fetchResult.json()
-		.then(
-			resultJson => {
-				switch (responseHandlingType) {
-					case "register":
-						if (resultJson.hasOwnProperty("authApproved")){
-							displayModal("Registration successful", `Welcome ${inputData.inputUsername}!`, "Global Exercises")}
-						else { invalidResponseHandling(resultJson, "register") }
-						break;
-					case "login":
-						if (resultJson.hasOwnProperty("authApproved"))
-							displayModal("Login successful", `Welcome back ${inputData.inputUsername}!`, "Global Exercises", true, "My Exercises")
-						else { invalidResponseHandling(resultJson, "login") }
-						break;
+	fetchResult.json().then(resultJson => {
+		switch (responseHandlingType) {
+			case "register":
+				if (resultJson.hasOwnProperty("authApproved")) {
+					displayModal(
+						"Registration successful",
+						`Welcome ${inputData.inputUsername}!`,
+						"Global Exercises"
+					);
+				} else {
+					invalidResponseHandling(resultJson, "register");
 				}
-			}
-		)
+				break;
+			case "login":
+				if (resultJson.hasOwnProperty("authApproved"))
+					displayModal(
+						"Login successful",
+						`Welcome back ${inputData.inputUsername}!`,
+						"Global Exercises",
+						true,
+						"My Exercises"
+					);
+				else {
+					invalidResponseHandling(resultJson, "login");
+				}
+				break;
+		}
+	});
 }
-				
-
-
 
 /**
  * Submitted form data is placed into body of fetch parameters.
@@ -120,13 +138,13 @@ function responseToJson(fetchResult, inputData, responseHandlingType) {
  * @param {object} inputData - Object created from submitted form data.
  */
 function fetchParameterInit(inputData) {
-    const fetchParameters = {
-        method: 'POST',
-        cors: '*same-origin',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(inputData)
-    }
-    return fetchParameters
+	const fetchParameters = {
+		method: "POST",
+		cors: "*same-origin",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(inputData)
+	};
+	return fetchParameters;
 }
 /**
  * Receives invalid inputs, removes start of the string and turns to lowercase.
@@ -136,24 +154,24 @@ function fetchParameterInit(inputData) {
  * @param {integer} isolationNumber - Number of letters to remove from string.
  */
 function authBooleanCheck(responseJSON, isolationNumber) {
-    invalidKeys = []
-    Object.keys(responseJSON).forEach((key) => {
-        if (!responseJSON[key]) {
-            invalidKeys.push((key.substr(isolationNumber)).toLowerCase())
-        }
-    })
-    return invalidKeys
+	invalidKeys = [];
+	Object.keys(responseJSON).forEach(key => {
+		if (!responseJSON[key]) {
+			invalidKeys.push(key.substr(isolationNumber).toLowerCase());
+		}
+	});
+	return invalidKeys;
 }
 /**
  * Uses jQuery to capture value of input data for workout spec.
  * Each value converted to lowercase for standardised database data.
  */
 function createExerciseObject() {
-    const inputData = {};
-    ($("input").each(function () {
-        inputData[this.id.toLowerCase()] = this.value.toLowerCase()
-    }))
-    return inputData
+	const inputData = {};
+	$("input").each(function() {
+		inputData[this.id.toLowerCase()] = this.value.toLowerCase();
+	});
+	return inputData;
 }
 
 /**
@@ -165,20 +183,24 @@ function createExerciseObject() {
  * @param {boolean} showCancelButton Used as toggle for right button display.
  * @param {string} cancelButtonTextString Text to be displayed in right button.
  */
-function displayModal(modalTitle, modalText = "", confirmButtonTextString = "Ok", showCancelButton, cancelButtonTextString="") {
-    Swal.fire({
-        title: modalTitle,
-        html: modalText,
-        confirmButtonText: confirmButtonTextString,
-        showCancelButton: showCancelButton,
-        cancelButtonText: cancelButtonTextString
-    })
-        .then((result) => {
-            if (result.value) {
-                window.location.replace("/globalexercises");
-            }
-            else if (result.dismiss) {
-                window.location.replace("/myexercises")
-            }
-        })
+function displayModal(
+	modalTitle,
+	modalText = "",
+	confirmButtonTextString = "Ok",
+	showCancelButton,
+	cancelButtonTextString = ""
+) {
+	Swal.fire({
+		title: modalTitle,
+		html: modalText,
+		confirmButtonText: confirmButtonTextString,
+		showCancelButton: showCancelButton,
+		cancelButtonText: cancelButtonTextString
+	}).then(result => {
+		if (result.value) {
+			window.location.replace("/globalexercises");
+		} else if (result.dismiss) {
+			window.location.replace("/myexercises");
+		}
+	});
 }
